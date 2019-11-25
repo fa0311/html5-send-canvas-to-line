fde6eb4e251b0a24e590e537e4a8e9f84e55a498
ふぁ <34892635+yukinashi@users.noreply.github.com>
2019-11-25 22:13:48 +0900

    Add files via upload

---
+++ liff/draw_save.php
@@ -0,0 +1,18 @@
+<?php
+$data = $_POST[data];
+$data = file_get_contents($data);
+touch('draw_system.json');
+$output = file_get_contents('draw_system.json');
+$output = (array)json_decode($output, true);
+$output["num"]++;
+$num = $output["num"];
+$directory_path = "draw";
+if (file_exists($directory_path)) {
+} else {
+    mkdir($directory_path, 0777);
+}
+file_put_contents('draw/draw'.$output["num"].'.png', $data, LOCK_EX);
+unset($output[$num-300]);
+$output = json_encode($output, (JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+$output = file_put_contents('draw_system.json', $output, LOCK_EX);
+echo "https://"."domain"."/liff/draw/draw".$num.".png";
